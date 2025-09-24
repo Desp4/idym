96ed3335a133bf81092a3b53b0d4c1b03938a140
Desp4 <desp4.kot@gmail.com>
2025-09-25 02:37:09 +0300

    start writing primary template tests

--- tests/expected.cpp
+++ tests/expected.cpp
@@ -99,10 +99,120 @@ void run_1_2() {
 }
 }
 
+// [expected.object.cons]
+namespace expected_object_cons {
+template<typename T, typename E>
+constexpr bool ex_default_cons_v = std::is_default_constructible<idym::expected<T, E>>::value;
+template<typename T, typename E>
+constexpr bool ex_copy_cons_v = std::is_copy_constructible<idym::expected<T, E>>::value;
+template<typename T, typename E>
+constexpr bool ex_trivial_copy_cons = std::is_trivially_copy_constructible<idym::expected<T, E>>::value;
+template<typename T, typename E>
+constexpr bool ex_move_cons_v = std::is_move_constructible<idym::expected<T, E>>::value;
+template<typename T, typename E>
+constexpr bool ex_trivial_move_cons_v = std::is_trivially_move_constructible<idym::expected<T, E>>::value;
+template<typename T, typename E>
+constexpr bool ex_nothrow_move_cons_v = std::is_nothrow_move_constructible<idym::expected<T, E>>::value;
+
+void run_1_5() {
+    static_assert(!ex_default_cons_v<idym_test::ndef_ctor, int>, "expected.object.cons.2");
+    static_assert(ex_default_cons_v<idym_test::def_ctor, int>, "expected.object.cons.2");
+    static_assert(ex_default_cons_v<idym_test::def_ctor_throws, int>, "expected.object.cons.2");
+
+    {
+        const idym::expected<idym_test::def_ctor, int> ex;
+        idym_test::validate(ex.has_value(), "expected.object.cons.4");
+        idym_test::validate(ex->value == 1337, "expected.object.cons.3");
+    }
+    IDYM_VALIDATE_EXCEPTION("expected.object.cons.5", idym::expected<idym_test::def_ctor_throws, int>{});
+}
+void run_6_10() {
+    static_assert(ex_copy_cons_v<idym_test::copy_ctor, idym_test::copy_ctor_throws>, "expected.object.cons.9");
+    static_assert(!ex_copy_cons_v<idym_test::ncopy_ctor, int>, "expected.object.cons.9");
+    static_assert(!ex_copy_cons_v<int, idym_test::ncopy_ctor>, "expected.object.cons.9");
+    static_assert(!ex_copy_cons_v<idym_test::ncopy_ctor, idym_test::ncopy_ctor>, "expected.object.cons.9");
+    
+    static_assert(ex_trivial_copy_cons<int, int>, "expected.object.cons.10");
+    static_assert(!ex_trivial_copy_cons<idym_test::copy_ctor, int>, "expected.object.cons.10");
+    static_assert(!ex_trivial_copy_cons<int, idym_test::copy_ctor>, "expected.object.cons.10");
+    static_assert(!ex_trivial_copy_cons<idym_test::copy_ctor, idym_test::copy_ctor>, "expected.object.cons.10");
+    
+    {
+        idym::expected<idym_test::copy_ctor, int> ex1{};
+        auto ex2 = ex1;
+        
+        idym_test::validate(ex2.has_value(), "expected.object.cons.7");
+        idym_test::validate(ex2.has_value() == ex1.has_value(), "expected.object.cons.7");
+        idym_test::validate(ex2->value == 1, "expected.object.cons.6");
+    }
+    {
+        idym::expected<int, idym_test::copy_ctor> ex1{idym::unexpect};
+        auto ex2 = ex1;
+        
+        idym_test::validate(!ex2.has_value(), "expected.object.cons.7");
+        idym_test::validate(ex2.has_value() == ex1.has_value(), "expected.object.cons.7");
+        idym_test::validate(ex2.error().value == 1, "expected.object.cons.6");
+    }
+    {
+        idym::expected<idym_test::copy_ctor_throws, int> ex1{};
+        IDYM_VALIDATE_EXCEPTION("expected.object.cons.9", idym::expected<idym_test::copy_ctor_throws, int>{ex1});
+    }
+    {
+        idym::expected<int, idym_test::copy_ctor_throws> ex1{idym::unexpect};
+        IDYM_VALIDATE_EXCEPTION("expected.object.cons.9", idym::expected<int, idym_test::copy_ctor_throws>{ex1});
+    }
+}
+void run_11_16() {
+    static_assert(ex_move_cons_v<idym_test::move_ctor, idym_test::move_ctor_throws>, "expected.object.cons.11");
+    static_assert(!ex_move_cons_v<idym_test::nmove_ctor, int>, "expected.object.cons.11");
+    static_assert(!ex_move_cons_v<int, idym_test::nmove_ctor>, "expected.object.cons.11");
+    static_assert(!ex_move_cons_v<idym_test::nmove_ctor, idym_test::nmove_ctor>, "expected.object.cons.11");
+    
+    static_assert(ex_trivial_move_cons_v<int, int>, "expected.object.cons.16");
+    static_assert(!ex_trivial_move_cons_v<idym_test::move_ctor, int>, "expected.object.cons.16");
+    static_assert(!ex_trivial_move_cons_v<int, idym_test::move_ctor>, "expected.object.cons.16");
+    static_assert(!ex_trivial_move_cons_v<idym_test::move_ctor, idym_test::move_ctor>, "expected.object.cons.16");
+    
+    static_assert(ex_nothrow_move_cons_v<idym_test::move_ctor, idym_test::move_ctor>, "expected.object.cons.16");
+    static_assert(!ex_nothrow_move_cons_v<idym_test::move_ctor_throws, idym_test::move_ctor>, "expected.object.cons.16");
+    static_assert(!ex_nothrow_move_cons_v<idym_test::move_ctor, idym_test::move_ctor_throws>, "expected.object.cons.16");
+    static_assert(!ex_nothrow_move_cons_v<idym_test::move_ctor_throws, idym_test::move_ctor_throws>, "expected.object.cons.16");
+    
+    {
+        idym::expected<idym_test::move_ctor, int> ex1{};
+        auto ex2 = std::move(ex1);
+        
+        idym_test::validate(ex2.has_value(), "expected.object.cons.13");
+        idym_test::validate(ex2.has_value() == ex1.has_value(), "expected.object.cons.13");
+        idym_test::validate(ex2->value == 1, "expected.object.cons.12");
+    }
+    {
+        idym::expected<int, idym_test::move_ctor> ex1{idym::unexpect};
+        auto ex2 = std::move(ex1);
+        
+        idym_test::validate(!ex2.has_value(), "expected.object.cons.13");
+        idym_test::validate(ex2.has_value() == ex1.has_value(), "expected.object.cons.13");
+        idym_test::validate(ex2.error().value == 1, "expected.object.cons.12");
+    }
+    {
+        idym::expected<idym_test::move_ctor_throws, int> ex1{};
+        IDYM_VALIDATE_EXCEPTION("expected.object.cons.14", idym::expected<idym_test::move_ctor_throws, int>{std::move(ex1)});
+    }
+    {
+        idym::expected<int, idym_test::move_ctor_throws> ex1{idym::unexpect};
+        IDYM_VALIDATE_EXCEPTION("expected.object.cons.14", idym::expected<int, idym_test::move_ctor_throws>{std::move(ex1)});
+    }
+}
+}
+
 int main(int, char**) {
     expected_un_cons::run_1_9();
     expected_un_obs::run_1_2();
     expected_un_eq::run_1_2();
+    
+    expected_object_cons::run_1_5();
+    expected_object_cons::run_6_10();
+    expected_object_cons::run_11_16();
 
     return 0;
 }
